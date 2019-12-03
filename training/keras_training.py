f3cb66508ae578e67a8f104619ae7f351ecb02af
Moritz Scham <moritz.scham@student.kit.edu>
2019-12-03 09:05:06 +0100

    yapf on kera_training

--- training/keras_training.py
+++ training/keras_training.py
@@ -15,15 +15,34 @@ def parse_arguments():
         description="Train machine Keras models for Htt analyses")
     parser.add_argument("config", help="Path to training config file")
     parser.add_argument("fold", type=int, help="Select the fold to be trained")
-    parser.add_argument("--conditional", required=False, type=bool, default=False, help="Use one network for all eras or separate networks.")
-    parser.add_argument("--randomization", required=False, type=bool, default=False, help="Randomize signal classes for conditional training in case one era has insufficient signal data.")
-    parser.add_argument("--balance-batches", required=False, type=bool, default=False, help="Use a equal amount of events of each class in a batch and normalize those by dividing each individual event weigth by the sum of event weight of the respective class in that batch ")
+    parser.add_argument(
+        "--conditional",
+        required=False,
+        type=bool,
+        default=False,
+        help="Use one network for all eras or separate networks.")
+    parser.add_argument(
+        "--randomization",
+        required=False,
+        type=bool,
+        default=False,
+        help=
+        "Randomize signal classes for conditional training in case one era has insufficient signal data."
+    )
+    parser.add_argument(
+        "--balance-batches",
+        required=False,
+        type=bool,
+        default=False,
+        help=
+        "Use a equal amount of events of each class in a batch and normalize those by dividing each individual event weigth by the sum of event weight of the respective class in that batch "
+    )
     return parser.parse_args()
 
 
 def parse_config(filename):
     logger.debug("Parse config.")
-    return yaml.load(open(filename, "r"),Loader=yaml.Loader)
+    return yaml.load(open(filename, "r"), Loader=yaml.Loader)
 
 
 def setup_logging(level, output_file=None):
@@ -66,7 +85,6 @@ def main(args, config):
     tfconfig.gpu_options.allow_growth = True
     set_session(tf.Session(config=tfconfig))
 
-
     from sklearn import preprocessing, model_selection
     import keras_models
     from keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
@@ -85,10 +103,10 @@ def main(args, config):
     else:
         eras = ['any']
         len_eras = 0
-    x = [] # Training input
-    y = [] # Target classes
-    w = [] # Weights for training
-    z = [] # Era information for batching
+    x = []  # Training input
+    y = []  # Target classes
+    w = []  # Weights for training
+    z = []  # Era information for batching
     classes = config["classes"]
     for i_era, era in enumerate(eras):
         if args.conditional:
@@ -106,8 +124,10 @@ def main(args, config):
             if tree == None:
                 logger.fatal("Tree %s not found in file %s.", class_, filename)
                 raise Exception
-            friend_trees_names = [k.GetName() for k in rfile.GetListOfKeys() if
-                                  k.GetName().startswith("_".join([class_, "friend"]))]
+            friend_trees_names = [
+                k.GetName() for k in rfile.GetListOfKeys()
+                if k.GetName().startswith("_".join([class_, "friend"]))
+            ]
             for friend in friend_trees_names:
                 tree.AddFriend(friend)
 
@@ -137,12 +157,13 @@ def main(args, config):
 
             # Get weights
             w_class = np.zeros((tree.GetEntries(), 1))
-            w_conv = root_numpy.tree2array(
-                tree, branches=[config["event_weights"]])
+            w_conv = root_numpy.tree2array(tree,
+                                           branches=[config["event_weights"]])
             if args.balance_batches:
                 w_class[:, 0] = w_conv[config["event_weights"]]
             else:
-                w_class[:, 0] = w_conv[config["event_weights"]] * config["class_weights"][class_]
+                w_class[:, 0] = w_conv[
+                    config["event_weights"]] * config["class_weights"][class_]
             w_era.append(w_class)
 
             # Get targets for this class
@@ -154,7 +175,7 @@ def main(args, config):
         x_era = np.vstack(x_era)  # inputs
         y_era = np.vstack(y_era)  # targets
         w_era = np.vstack(w_era)  # weights
-        z_era = np.zeros((y_era.shape[0], len(eras))) # era information
+        z_era = np.zeros((y_era.shape[0], len(eras)))  # era information
         z_era[:, i_era] = np.ones((y_era.shape[0]))
         x.append(x_era)
         y.append(y_era)
@@ -166,9 +187,7 @@ def main(args, config):
     y = np.vstack(y)  # targets
     w = np.vstack(w)  # weights
     w = np.squeeze(w)  # needed to get weights into keras
-    z = np.vstack(z) # era information
-
-
+    z = np.vstack(z)  # era information
 
     # Perform input variable transformation and pickle scaler object.
     # Only perform transformation on continuous variables
@@ -193,7 +212,8 @@ def main(args, config):
             logger.debug("Preprocessing (variable, mean, std): %s, %s, %s",
                          var, mean, std)
     elif "min_max_scaler" in config["preprocessing"]:
-        scaler = preprocessing.MinMaxScaler(feature_range=(-1.0, 1.0)).fit(x_scaler)
+        scaler = preprocessing.MinMaxScaler(feature_range=(-1.0,
+                                                           1.0)).fit(x_scaler)
         for var, min_, max_ in zip(variables, scaler.data_min_,
                                    scaler.data_max_):
             logger.debug("Preprocessing (variable, min, max): %s, %s, %s", var,
@@ -206,7 +226,7 @@ def main(args, config):
         logger.fatal("Preprocessing %s is not implemented.",
                      config["preprocessing"])
         raise Exception
-    x[:,:len(variables)] = scaler.transform(x_scaler)
+    x[:, :len(variables)] = scaler.transform(x_scaler)
 
     path_preprocessing = os.path.join(
         config["output_path"],
@@ -222,7 +242,7 @@ def main(args, config):
         z,
         test_size=1.0 - config["train_test_split"],
         random_state=int(config["seed"]))
-    del x,y,w
+    del x, y, w
     # Add callbacks
     callbacks = []
     if "early_stopping" in config["model"]:
@@ -243,8 +263,8 @@ def main(args, config):
         logger.info("Reduce learning-rate after %s tries.",
                     config["model"]["reduce_lr_on_plateau"])
         callbacks.append(
-            ReduceLROnPlateau(
-                patience=config["model"]["reduce_lr_on_plateau"], verbose=1))
+            ReduceLROnPlateau(patience=config["model"]["reduce_lr_on_plateau"],
+                              verbose=1))
 
     # Train model
     if not hasattr(keras_models, config["model"]["name"]):
@@ -258,44 +278,92 @@ def main(args, config):
         batch_size = config["model"]["batch_size"]
 
     ###
-    classIndexDict={label:np.where(y_train[:,i_class]==1)[0] for i_class,label in enumerate(classes)}
+    classIndexDict = {
+        label: np.where(y_train[:, i_class] == 1)[0]
+        for i_class, label in enumerate(classes)
+    }
     if "steps_per_epoch" in config["model"]:
-        steps_per_epoch=int(config["model"]["steps_per_epoch"])
-        recommend_steps_per_epoch=int(min([len(classIndexDict[class_]) for class_ in classes])/(batch_size/len(classes)))+1
-        logger.info("steps_per_epoch: Using {} instead of recommended minimum of {}".format(str(steps_per_epoch), str(recommend_steps_per_epoch)))
+        steps_per_epoch = int(config["model"]["steps_per_epoch"])
+        recommend_steps_per_epoch = int(
+            min([len(classIndexDict[class_])
+                 for class_ in classes]) / (batch_size / len(classes))) + 1
+        logger.info(
+            "steps_per_epoch: Using {} instead of recommended minimum of {}".
+            format(str(steps_per_epoch), str(recommend_steps_per_epoch)))
     else:
-        logger.info("model: steps_per_epoch: Not found in {} ".format(args.config))
+        logger.info("model: steps_per_epoch: Not found in {} ".format(
+            args.config))
         raise Exception
 
     model_impl = getattr(keras_models, config["model"]["name"])
     model = model_impl(len(variables) + len_eras, len(classes))
     model.summary()
-    if(args.balance_batches):
+    if (args.balance_batches):
         logger.info("Running on balanced batches.")
         # Loop over all eras and classes to divide the batch equally, defines the indices of the corrects answers for each era/class combination
-        eraIndexDict={era: {label: np.where((z_train[:,i_era] == 1) & (y_train[:,i_class] == 1))[0] for i_class, label in enumerate(classes)} for i_era, era in enumerate(eras)}
+        eraIndexDict = {
+            era: {
+                label: np.where((z_train[:, i_era] == 1)
+                                & (y_train[:, i_class] == 1))[0]
+                for i_class, label in enumerate(classes)
+            }
+            for i_era, era in enumerate(eras)
+        }
+
         def balancedBatchGenerator(batch_size):
             while True:
-                nperClass=int(batch_size/(len(classes)*len(eras)))
-                selIdxDict={era: {label:eraIndexDict[era][label][np.random.randint(0,len(eraIndexDict[era][label]),nperClass)] for label in classes} for era in eras}
-                y_collect=np.concatenate([y_train[selIdxDict[era][label]] for label in classes for era in eras])
-                x_collect=np.concatenate([x_train[selIdxDict[era][label],:] for label in classes for era in eras])
-                w_collect=np.concatenate([w_train[selIdxDict[era][label]]*(batch_size/np.sum(w_train[selIdxDict[era][label]])) for label in classes for era in eras])
-                yield x_collect, y_collect,w_collect
+                nperClass = int(batch_size / (len(classes) * len(eras)))
+                selIdxDict = {
+                    era: {
+                        label: eraIndexDict[era][label][np.random.randint(
+                            0, len(eraIndexDict[era][label]), nperClass)]
+                        for label in classes
+                    }
+                    for era in eras
+                }
+                y_collect = np.concatenate([
+                    y_train[selIdxDict[era][label]] for label in classes
+                    for era in eras
+                ])
+                x_collect = np.concatenate([
+                    x_train[selIdxDict[era][label], :] for label in classes
+                    for era in eras
+                ])
+                w_collect = np.concatenate([
+                    w_train[selIdxDict[era][label]] *
+                    (batch_size / np.sum(w_train[selIdxDict[era][label]]))
+                    for label in classes for era in eras
+                ])
+                yield x_collect, y_collect, w_collect
 
         def calculateValidationWeights(x_test, y_test, w_test):
             testIndexDict = {
-                era: {label: np.where((z_test[:, i_era] == 1) & (y_test[:, i_class] == 1))[0] for i_class, label in
-                      enumerate(classes)} for i_era, era in enumerate(eras)}
+                era: {
+                    label: np.where((z_test[:, i_era] == 1)
+                                    & (y_test[:, i_class] == 1))[0]
+                    for i_class, label in enumerate(classes)
+                }
+                for i_era, era in enumerate(eras)
+            }
 
-            y_collect = np.concatenate([y_train[testIndexDict[era][label]] for label in classes for era in eras])
-            x_collect = np.concatenate([x_train[testIndexDict[era][label], :] for label in classes for era in eras])
-            w_collect = np.concatenate(
-                [w_test[testIndexDict[era][class_]] * (len(x_test) / np.sum(w_test[testIndexDict[era][class_]])) for class_ in classes for era in eras])
+            y_collect = np.concatenate([
+                y_train[testIndexDict[era][label]] for label in classes
+                for era in eras
+            ])
+            x_collect = np.concatenate([
+                x_train[testIndexDict[era][label], :] for label in classes
+                for era in eras
+            ])
+            w_collect = np.concatenate([
+                w_test[testIndexDict[era][class_]] *
+                (len(x_test) / np.sum(w_test[testIndexDict[era][class_]]))
+                for class_ in classes for era in eras
+            ])
 
             return x_collect, y_collect, w_collect
 
-        x_test, y_test, w_test = calculateValidationWeights(x_test, y_test, w_test)
+        x_test, y_test, w_test = calculateValidationWeights(
+            x_test, y_test, w_test)
 
         history = model.fit_generator(
             balancedBatchGenerator(batch_size=batch_size),
@@ -305,30 +373,31 @@ def main(args, config):
             validation_data=(x_test, y_test, w_test),
             max_queue_size=10,
             workers=5,
-            )
+        )
 
     else:
-        history = model.fit(
-            x_train,
-            y_train,
-            sample_weight=w_train,
-            validation_data=(x_test, y_test, w_test),
-            batch_size=batch_size,
-            epochs=config["model"]["epochs"],
-            shuffle=True,
-            callbacks=callbacks)
+        history = model.fit(x_train,
+                            y_train,
+                            sample_weight=w_train,
+                            validation_data=(x_test, y_test, w_test),
+                            batch_size=batch_size,
+                            epochs=config["model"]["epochs"],
+                            shuffle=True,
+                            callbacks=callbacks)
 
     # Plot loss
     epochs = range(1, len(history.history["loss"]) + 1)
     plt.plot(epochs, history.history["loss"], lw=3, label="Training loss")
-    plt.plot(
-        epochs, history.history["val_loss"], lw=3, label="Validation loss")
+    plt.plot(epochs,
+             history.history["val_loss"],
+             lw=3,
+             label="Validation loss")
     plt.xlabel("Epoch"), plt.ylabel("Loss")
     path_plot = os.path.join(config["output_path"],
                              "fold{}_loss".format(args.fold))
     plt.legend()
-    plt.savefig(path_plot+".png", bbox_inches="tight")
-    plt.savefig(path_plot+".pdf", bbox_inches="tight")
+    plt.savefig(path_plot + ".png", bbox_inches="tight")
+    plt.savefig(path_plot + ".pdf", bbox_inches="tight")
 
     # Save model
     if not "save_best_only" in config["model"]:
@@ -339,5 +408,6 @@ def main(args, config):
 if __name__ == "__main__":
     args = parse_arguments()
     config = parse_config(args.config)
-    setup_logging(logging.DEBUG, "{}/training{}.log".format(config["output_path"], args.fold))
+    setup_logging(logging.DEBUG,
+                  "{}/training{}.log".format(config["output_path"], args.fold))
     main(args, config)
